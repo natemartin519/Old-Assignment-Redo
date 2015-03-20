a3a6326c02431c42b0eb158e87c12b5bb0447bc6
natemartin519 <natemartin519@gmail.com>
2015-03-20 14:39:33 -0400

    Inital Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Collage Assignment Redo
+
+###Background
+I was looking at an old collage assignment and didn't like what i was seen. Redoing it to follow more modern and clean PHP practices.
+
+###Restrictions
+No frameworks or packages are allowed for the main app. 
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php require 'vendor/autoload.php';?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>AJAX Assignment</title>
+</head>
+<body>
+
+</body>
+    <div class="container">
+        <div class="body">
+            <h1>Ajax Assignment</h1>
+        </div>
+    </div>
+</html>
